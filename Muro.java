af51e82c34f85ef078c6f5e7e140b1e0e4952916
Carlos Moro <cmoroflorez@gmail.com>
2017-03-13 04:25:20 +0100

    clase Muro añadidos métodos

--- Muro.java
+++ Muro.java
@@ -1,33 +1,27 @@
+import java.util.ArrayList;
 
-/**
- * Write a description of class Muro here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Muro
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Muro
-     */
+    private ArrayList<EntradaTexto> mensajes;
+    private ArrayList<EntradaFoto> fotos;
+    
     public Muro()
     {
-        // initialise instance variables
-        x = 0;
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
+    
+    public void addEntradaTexto(EntradaTexto entradaTexto)
     {
-        // put your code here
-        return x + y;
+        
+    }
+    
+    public void addEntradaFoto(EntradaFoto entradaFoto)
+    {
+    
+    }
+    
+    public String toString()
+    {
+        return null;
     }
 }
