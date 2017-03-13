8f11935b2ea3ebdddfbae0516f2bff9db280931b
Carlos Moro <cmoroflorez@gmail.com>
2017-03-13 04:21:28 +0100

    clase EntradaFoto añadidos métodos

--- EntradaFoto.java
+++ EntradaFoto.java
@@ -1,33 +1,54 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
-/**
- * Write a description of class EntradaFoto here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class EntradaFoto
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private String usuario;
+    private String urlImagen;
+    private String titulo;
+    private LocalDateTime momentoPublicacion;
+    private int cantidadMeGusta;
+    private ArrayList<String> comentarios;
 
-    /**
-     * Constructor for objects of class EntradaFoto
-     */
-    public EntradaFoto()
+    public EntradaFoto(String usuario, String url, String titulo)
     {
-        // initialise instance variables
-        x = 0;
+        this.usuario = usuario;
+        this.titulo = titulo;
+        urlImagen = url;
+        momentoPublicacion = LocalDateTime.now();
+        cantidadMeGusta = 0;
+        comentarios = new ArrayList<String>();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
+    public void meGusta()
     {
-        // put your code here
-        return x + y;
+        
+    }
+    
+    public void addComentario(String texto)
+    {
+    
+    }
+    
+    public String getUrlImagen()
+    {
+        return urlImagen;
+    }
+    
+    public String getTituloDeLaImagen()
+    {
+        return titulo;
+    }
+    
+    public LocalDateTime getMomentoPublicacion()
+    {
+        return momentoPublicacion;
+    }
+    
+    public String toString()
+    {
+        String textoADevolver = null;
+        
+        return textoADevolver;
     }
 }
